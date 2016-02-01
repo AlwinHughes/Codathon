7143248c12a9d93e973dfe842c95f7a17838de8a
Alwin Hughes <alwin.hughes@btopenworld.com>
2016-02-01 16:28:54 +0000

    Started work towards making it object orientated but all of it other than this is breaking

---
+++ Example_name/Example_name/Thing.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Example_name
+{
+    class Thing
+    {
+        public Thing() { }
+
+        public int x, y;
+        public float rotation;
+        public Texture2D texture;
+        public Color[] color;
+        public int width;
+        public int height;
+
+        public Thing(int x, int y,Texture2D texture,int array_x, int array_y,float rotation)
+        {
+            this.x = x;
+            this.y = y;
+            this.texture = texture; 
+            color = new Color[array_x * array_y];
+        }
+      
+
+
+    }
+}
