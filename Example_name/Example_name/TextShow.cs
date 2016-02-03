ae09e02879c9df1a86aa92a71dae8a6bb7abcb41
Alwin Hughes <alwin.hughes@btopenworld.com>
2016-02-03 21:24:52 +0000

    re added method that tom has deleted about 10 times thanks tom

--- Example_name/Example_name/TextShow.cs
+++ Example_name/Example_name/TextShow.cs
@@ -67,5 +67,10 @@ namespace Example_name
             Game1.spriteBatch.Draw(texture, new Rectangle((int)location.X, (int)location.Y, texture.Width, texture.Height), Color.White);
             Game1.spriteBatch.DrawString(font, text, new Vector2(location.X + border_size , location.Y + border_size), text_color);
         }
+
+        public Vector2 getOffset()
+        {
+            return new Vector2(4 + border_size + sprite_length / 2, 4 + border_size + sprite_height / 2);
+        }
     }
 }
